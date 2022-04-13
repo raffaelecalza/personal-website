8858712824a8067eb0cee531cd1eaeb2823ca0e4
Raffaele Calzà <raffaelecalza4@gmail.com>
2022-04-13 14:50:36 +0200

    fixed some graphical issues

--- assets/js/others.js
+++ assets/js/others.js
@@ -1,6 +1,6 @@
 document.addEventListener('keydown', function(e) {
     var key = (!e.altKey && !e.ctrlKey && !e.metaKey) ? e.keyCode : null
     if(key == 84) { // Back to top
-        document.getElementById('content').scrollIntoView()
+        document.querySelector('html').scrollIntoView()
     }
 })
